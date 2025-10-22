62f105926693825ff1c4f810186865283205d84c
KarunaSri Gaddam <gaddamkarunasri679@gmail.com>
2025-10-22 22:03:38 +0530

    life cylce in class component

--- project/src/components/classComponent.jsx
+++ project/src/components/classComponent.jsx
@@ -1,5 +1,33 @@
 import { Component } from "react";
 
+/**
+ * Life Cycle Methods in Class Component
+ * Mount --> Update --> Unmount
+ */
+
+// when a coponent is mounted (i.e first created in DOM), it runs only once
+componentDidMount() {
+  //   fetch data from an API
+  fetch("https://jsonplaceholder.typicode.com/posts")
+    .then((response) => response.json())
+    .then((data) => console.log(data.slice(0, 2)));
+}
+
+// it checks the nextProps ans current state and returns true or false
+shouldComponentUpdate(nextProps, nextState) {
+  return true;
+}
+
+// it runs when the component is updated
+componentDidUpdate() {}
+
+// it runs when the component is unmounted
+// like we did in timer example like in useEffect
+componentWillUnmount() {}
+
+// it runs when the component is mounted 
+componentDidMount() {}
+
 class Welcome extends Component {
   render() {
     return <h1>Hello, {this.props.name}</h1>;
