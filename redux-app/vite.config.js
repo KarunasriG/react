5f3db850432f1e89797088626e72bde774572afd
KarunaSri Gaddam <gaddamkarunasri679@gmail.com>
2025-10-26 23:27:31 +0530

    base path modified

--- redux-app/vite.config.js
+++ redux-app/vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/redux-app/", // 👈 subpath for this app
+  base: "/react/redux-app/", // 👈 subpath for this app
 });
