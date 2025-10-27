1d975d61fe0579935fa18c90dfa2d8e14786fa8d
KarunaSri Gaddam <gaddamkarunasri679@gmail.com>
2025-10-27 16:14:12 +0530

    commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/react/project/", // 👈 important for GitHub Pages subpath
+  base: "/react/", // 👈 important for GitHub Pages subpath
 });
